3bcc54d5afeb13a826e10afaf49ca4189f303217
19shubh99 <37965732+19shubh99@users.noreply.github.com>
2019-10-31 13:25:17 +0530

    Update server.py
    error induced: client side, identified: server side

--- server.py
+++ server.py
@@ -18,26 +18,52 @@ print("Server is up and running")
 while True:
      c, addr = s.accept() 		# Establish connection with client.
      print('Got connection from', addr)
+     print("\n ~ is corrsponding to the errorneous bit")
      count=0
      Num=0
      while True:
-        equation=c.recv(1024).decode()
-        if equation == "Q" or equation == "q" or equation == "Quit" or equation == "quit" or equation == "quit()":
+        #recieve the final string (error induced)
+        message=c.recv(1024).decode()
+
+        #recieve the xor string
+        parity=c.recv(1024).decode()
+        j=0
+
+        # print(message)
+        # print(parity)
+
+        if message == "Q" or message == "q" or message == "Quit" or message == "quit" or message == "quit()":
             c.send("Quit".encode())
             break
         else:
-            #Result=(Decode(equation))
-            #print(equation)
-            Num=0
-            for i  in  range(5):
-                Num*=2
-                if(equation[i]=="1"):
-                    Num+=1
-            print(Decode(Num-1))
+            
+            #roving over the parity string
+            for j in range(0,len(parity)):
+
+                #to store the number
+                Num=0
+
+                #to store the corresponding temp. xor
+                tmp=0
+
+                #roving over the corresponding message string
+                for i  in  range(j*6,j*6+6):
+                    Num*=2
+                    if(message[i]=="1"):
+                        Num+=1
+                        tmp^=1
+                    else:
+                        tmp^=0
+                
+                if(parity[j]!=str(tmp)):
+                    #corresponding to the erroreneous bit
+                    print("~",Decode(Num-1))
+                else:
+                    print(Decode(Num-1))
             
 
-            
-            c.send(equation.encode())
+            print("\n")
+            c.send(message.encode())
           
 
      c.close() 			# Close the connection.
