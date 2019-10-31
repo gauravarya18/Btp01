e34eaed4955256791715d960e018a58af97ba412
19shubh99 <37965732+19shubh99@users.noreply.github.com>
2019-10-31 13:23:53 +0530

    Update client.py
    error induced: client side, identified: server side

--- client.py
+++ client.py
@@ -2,16 +2,14 @@ import socket		 	 # Import socket module
 import sys
 import ipaddress
 import queue 
-temp = queue.LifoQueue(maxsize=5)
+temp = queue.LifoQueue(maxsize=6)
 def Convert_Binary(x):
     #print(x)
     while(x>0):
         temp.put(x%2)
         x//=2
-        #print(x)
-    #print(x)
-    #print(temp.qsize())
-    while(temp.qsize()<5):
+
+    while(temp.qsize()<6):
         temp.put(0)
 
 
@@ -30,7 +28,9 @@ def  Encoding(equ):
                 L.put(x+1)
         
         
-L = queue.Queue(maxsize=20) 
+L = queue.Queue(maxsize=20)
+
+
 s = socket.socket() 	  		 # Create a socket object
 hostname = socket.gethostname()    
 IPAddr = socket.gethostbyname(hostname)
@@ -41,40 +41,69 @@ print("The IP address of the server is:", host)
 print("The port number of the server is:", port)
 qu="quit"
 while(True):
-    equ=input("Please give me your equation (Ex: 2+2) or Q to quit: ")
+    equ=input("\n Please provide the input or Q to quit: ")
     Encoding(equ)
+    
+    #binary of the whole message
+    strfinal=""
+
+    #stores the parity
+    xorfinal=""
+
     while(L.qsize()>0):
         p=L.get()
-        #print("h0")
-        #print(p)
+        
         if(p==-1):
             s.send(qu.encode())
         else:
             Convert_Binary(p)
-            print("h1")
+            
+            #temporary binary conversion
             str1=""
-            for i in  range(5):
+
+            #storing the xor of current slot of 6
+            tmp=0
+            for i in  range(6):
+
                 y=temp.get()
                 if(y==0):
                     str1+="0"
+                    tmp^=0
                 else:
                     str1+="1"
-                print(y)
-            s.send(str1.encode())
-    #print("h2")
+                    tmp^=1
+                
+            #reset
+            temp = queue.LifoQueue(maxsize=6)
+            
+            strfinal+=str1
+            xorfinal+=str(tmp)
+            
+    
+
+    #induce error
+    inp=input("Position you want to induce error in ")
+
+    x=int(strfinal[int(inp)])
+    x=(x+1)%2
+
+    #error induced message at position inp
+    strToSend=strfinal[:int(inp)]+str(x)+strfinal[int(inp)+1:]
+
+
+    # print(xorfinal)
+    # print(strfinal)
+    
+    s.send(strToSend.encode())
+    s.send(xorfinal.encode())
+
+
     result = s.recv(1024).decode()       # 1024 is recv_size
     if result == "Quit":
         print("Closing client connection, goodbye")
         break
-    elif result == "ZeroDiv":
-        print("You can't divide by 0, try again")
-    elif result == "MathError":
-        print("There is an error with your math, try again")
-    elif result == "SyntaxError":
-        print("There is a syntax error, please try again")
-    elif result == "NameError":
-        print("You did not enter an equation, try again")
     else:
+        print("orgnl message:", strfinal)
         print("The answer is:", result)
 
 s.close 				 # Close the socket when done
